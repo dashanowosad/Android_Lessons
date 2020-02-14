fac2fb22bd1213ea0b3bb1b4d6bf2f168e7d3325
Dasha <dashanowosad@yandex.ru>
2020-02-14 22:51:56 +0700

    Create First android lesson (basics)

---
+++ First/app/src/main/java/com/example/first/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.first;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Spinner;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private BeerExpert expert = new BeerExpert();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickFindBeer(View view){
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        //brands.setText(beerType);
+
+        List<String> brandList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for(String brand : brandList){
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+
+}
